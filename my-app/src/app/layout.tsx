4fb080e0a2a4057db0382e2f182a9a71f4bf0cd9
imzeze <jihyekim019@gmail.com>
2025-08-31 20:55:12 +0900

    feat: init

---
+++ my-app/src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Geist } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "zeze 단어집",
+  description: "매일 쓰는 단어집",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
